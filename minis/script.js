909e87baf5af8524e5381ad09bcf831a8235e020
Leland Capps <lcapps5600@gmail.com>
2023-08-22 13:03:12 -0500

    yessir

--- minis/script.js
+++ minis/script.js
@@ -136,3 +136,168 @@
 
 // console.log(addArray([1, 1, 1, 1]))
 // console.log(addArray([1, 2, 3, 4, 5]))
+
+
+
+// Unique characters using set()
+
+// function unique(str){
+
+//     return new Set(str).size === str.length
+
+//     // let tempStr = new Set()
+//     // for(letter of str){
+//     //     if(tempStr.has(letter)){
+//     //         return false
+//     //     }
+//     //     tempStr.add(letter)
+//     // }
+//     // return true
+
+// }
+
+
+// console.log(unique('abcdefg'))
+// console.log(unique('abcdeafg'))
+
+
+// filter products by company 
+
+// let products = [
+//     {
+//         title: 'Iphone 8',
+//         company: 'Apple'
+//     },
+//     {
+//         title: 'Galaxy',
+//         company: 'Samsung'
+//     },
+//     {
+//         title: 'Iphone 7',
+//         company: 'Apple'
+//     },
+//     {
+//         title: 'Iphone xS',
+//         company: 'Apple'
+//     },
+//     {
+//         title: 'HTC phone',
+//         company: 'HTC'
+//     }
+// ]
+
+// function getUnique(arr){
+//     // let tempArr = arr.map(item => {
+//     //     return item.company
+//     // })
+//     // return [... new Set(tempArr)]
+//     return [...arr.reduce((acc,curr) => {
+//         acc.add(curr.company)
+//         return acc
+//     }, new Set())]
+// }
+
+// console.log(getUnique(products))
+
+
+
+
+// word with most repeated letters
+
+// function repeatLetter(str){
+//     let tempArr = str.split(' ')
+//     tempArr = tempArr.map(item => {
+//         let tempItem = item.toLowerCase().split('')
+//         return tempItem.reduce((acc,curr) => {
+//             acc[curr] = acc[curr] ? acc[curr] + 1 : 1
+//             if(acc[curr]>acc.max){
+//                 acc.max = acc[curr]
+//             }
+//             return acc
+
+//         }, {max:1,word:item})
+
+//     })
+//     let amount = 1 
+//     let word = ''
+//     for (let item of tempArr){
+//         if(item.max > amount){
+//             amount = item.max
+//             word = item.word
+//         }
+//     }
+//     if(amount > 1){
+
+//         return word
+//     }
+//     return -1
+
+
+
+
+//     return tempArr
+// }
+
+// console.log(repeatLetter('welcome to the great state of mississippi'))
+// console.log(repeatLetter('Javascript is the best programming language ever'))
+
+
+// is a prime number?
+
+// function isPrimeNum(num){
+//     if (num < 2){
+//         return false
+//     }
+
+//     return num
+// }
+
+// console.log(isPrimeNum(7))
+// console.log(isPrimeNum(12))
+
+
+// is string a palindrome wiht and without Regex
+
+// function isPalindrome(str){
+//     specialChar = ['!', '@', '#', "$", '%', '^', "*", '(', ')']
+//     let noRegex = str.match(/[a-z0-9]+/ig)
+    
+//     let tempStr = str.split(' ').join('')
+//     let backStr = tempStr.split('').reverse().join('')
+//     console.log(tempStr)
+//     console.log(backStr)
+    
+    // for (let i=tempStr.length-1; i>=0; i--){
+    //     if(!specialChar.includes(tempStr[i])){
+
+    //         backStr.push(tempStr[i])
+    //     }
+    // }
+
+    // backStr = backStr.join('')
+
+    // if (backStr === tempStr){
+    //     return true
+    // }
+    // return false
+// }
+
+
+// console.log(isPalindrome('was it a car or a cat i saw'))
+
+
+
+
+
+function chunkyArr(arr, len){
+    const chunkedArr = []
+    let i=0
+    while(i<arr.length){
+        chunkedArr.push(arr.slice(i, i+len))
+        i += len
+    }
+    return chunkedArr
+}
+
+console.log(chunkyArr([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11], 3))
+
